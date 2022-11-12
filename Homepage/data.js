fb4bc526eb1faea6a12ab9961b05804f4f026378
Krishna-7777 <kchhabraji13@gmail.com>
2022-11-12 23:43:19 +0530

    day-5

--- Homepage/data.js
+++ Homepage/data.js
@@ -11,283 +11,323 @@ let data=[
           "maxHeight": 933,
           "maxWidth": 1400,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/c9/d8/8c/hotel-exterior.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Delhi Bed and Breakfast</b>",
-        "geoId": "594461",
-        "documentId": "594461",
-        "trackingItems": "hotel",
-        "secondaryText": "New Delhi, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Delhi Bed and Breakfast</b>",
+      "geoId": "594461",
+      "documentId": "594461",
+      "trackingItems": "hotel",
+      "secondaryText": "New Delhi, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 370,
           "maxWidth": 639,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/04/c0/42/d5/delhi-bed-and-breakfast.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Delhieez - A Boutique Hotel</b>",
-        "geoId": "21139133",
-        "documentId": "21139133",
-        "trackingItems": "hotel",
-        "secondaryText": "New Delhi, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Delhieez - A Boutique Hotel</b>",
+      "geoId": "21139133",
+      "documentId": "21139133",
+      "trackingItems": "hotel",
+      "secondaryText": "New Delhi, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 3000,
           "maxWidth": 4000,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/8b/05/15/delhieez-a-boutique-hotel.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Delhi Darbar Inn</b>",
-        "geoId": "7291335",
-        "documentId": "7291335",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Delhi Darbar Inn</b>",
+      "geoId": "7291335",
+      "documentId": "7291335",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 916,
           "maxWidth": 1632,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/5a/01/b0/front-of-the-delhi-darbar.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Delhi Darbar Mount Hall</b>",
-        "geoId": "3534978",
-        "documentId": "3534978",
-        "trackingItems": "hotel",
-        "secondaryText": "Panchgani, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Delhi Darbar Mount Hall</b>",
+      "geoId": "3534978",
+      "documentId": "3534978",
+      "trackingItems": "hotel",
+      "secondaryText": "Panchgani, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1275,
           "maxWidth": 831,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/bb/2b/df/mount-hall-apartments.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai House</b>",
-        "geoId": "13085455",
-        "documentId": "13085455",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai House</b>",
+      "geoId": "13085455",
+      "documentId": "13085455",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 640,
           "maxWidth": 960,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/25/7f/23/mumbai-house.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai House, Airoli- Navi Mumbai</b>",
-        "geoId": "6165303",
-        "documentId": "6165303",
-        "trackingItems": "hotel",
-        "secondaryText": "Navi Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai House, Airoli- Navi Mumbai</b>",
+      "geoId": "6165303",
+      "documentId": "6165303",
+      "trackingItems": "hotel",
+      "secondaryText": "Navi Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1333,
           "maxWidth": 2000,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/a1/fd/b7/mango-hotels-navi-mumbai.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai House</b>",
-        "geoId": "15113246",
-        "documentId": "15113246",
-        "trackingItems": "hotel",
-        "secondaryText": "Udaipur, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai House</b>",
+      "geoId": "15113246",
+      "documentId": "15113246",
+      "trackingItems": "hotel",
+      "secondaryText": "Udaipur, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 657,
           "maxWidth": 960,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/7b/ba/ff/landmark-hotel.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai House Luxury Apartment</b>",
-        "geoId": "17622565",
-        "documentId": "17622565",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai House Luxury Apartment</b>",
+      "geoId": "17622565",
+      "documentId": "17622565",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 3648,
           "maxWidth": 5472,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/50/5f/e2/mumbai-house-luxury-apartments.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai Metro - The Executive Hotel</b>",
-        "geoId": "3590708",
-        "documentId": "3590708",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai Metro - The Executive Hotel</b>",
+      "geoId": "3590708",
+      "documentId": "3590708",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 3024,
           "maxWidth": 4032,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/9f/3d/5b/suite-room.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai Staytion Dorm - A Backpackers Hostel</b>",
-        "geoId": "14956195",
-        "documentId": "14956195",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai Staytion Dorm - A Backpackers Hostel</b>",
+      "geoId": "14956195",
+      "documentId": "14956195",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 2178,
           "maxWidth": 3265,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/0e/f7/75/mumbai-staytion.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai House Goa</b>",
-        "geoId": "17381784",
-        "documentId": "17381784",
-        "trackingItems": "hotel",
-        "secondaryText": "Calangute, Goa, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai House Goa</b>",
+      "geoId": "17381784",
+      "documentId": "17381784",
+      "trackingItems": "hotel",
+      "secondaryText": "Calangute, Goa, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 2144,
           "maxWidth": 4095,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/ff/15/40/mumbai-house-goa.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Mumbai Backpackers</b>",
-        "geoId": "12819513",
-        "documentId": "12819513",
-        "trackingItems": "hotel",
-        "secondaryText": "Mumbai, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Mumbai Backpackers</b>",
+      "geoId": "12819513",
+      "documentId": "12819513",
+      "trackingItems": "hotel",
+      "secondaryText": "Mumbai, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1125,
           "maxWidth": 2000,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/1a/9c/13/mumbai-backpackers.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Bangalore Inn</b>",
-        "geoId": "12169731",
-        "documentId": "12169731",
-        "trackingItems": "hotel",
-        "secondaryText": "Bengaluru, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Bangalore Inn</b>",
+      "geoId": "12169731",
+      "documentId": "12169731",
+      "trackingItems": "hotel",
+      "secondaryText": "Bengaluru, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1032,
           "maxWidth": 774,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/d2/9b/df/20180501-225842-largejpg.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Hotel Bangalore Gate</b>",
-        "geoId": "624563",
-        "documentId": "624563",
-        "trackingItems": "hotel",
-        "secondaryText": "Bengaluru, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Hotel Bangalore Gate</b>",
+      "geoId": "624563",
+      "documentId": "624563",
+      "trackingItems": "hotel",
+      "secondaryText": "Bengaluru, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 641,
           "maxWidth": 960,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/7a/36/0c/hotel-bangalore-gate.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Bangalore Rooms - Whitefield</b>",
-        "geoId": "1060401",
-        "documentId": "1060401",
-        "trackingItems": "hotel",
-        "secondaryText": "Bengaluru, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Bangalore Rooms - Whitefield</b>",
+      "geoId": "1060401",
+      "documentId": "1060401",
+      "trackingItems": "hotel",
+      "secondaryText": "Bengaluru, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1080,
           "maxWidth": 1920,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/69/47/3a/bangalore-rooms-whitefield.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Kashmir Mahal Resorts</b>",
-        "geoId": "10025445",
-        "documentId": "10025445",
-        "trackingItems": "hotel",
-        "secondaryText": "Srinagar, Kashmir, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Kashmir Mahal Resorts</b>",
+      "geoId": "10025445",
+      "documentId": "10025445",
+      "trackingItems": "hotel",
+      "secondaryText": "Srinagar, Kashmir, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 858,
           "maxWidth": 1280,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/bf/31/ff/kashmir-mahal-resorts.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Kashmir Group of Houseboats</b>",
-        "geoId": "4663163",
-        "documentId": "4663163",
-        "trackingItems": "hotel",
-        "secondaryText": "Srinagar, Kashmir, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Kashmir Group of Houseboats</b>",
+      "geoId": "4663163",
+      "documentId": "4663163",
+      "trackingItems": "hotel",
+      "secondaryText": "Srinagar, Kashmir, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1920,
           "maxWidth": 2880,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/df/b2/4f/kashmir-group-of-houseboats.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>The Kashmir House</b>",
-        "geoId": "1463513",
-        "documentId": "1463513",
-        "trackingItems": "hotel",
-        "secondaryText": "Dharamsala, Himachal Pradesh, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>The Kashmir House</b>",
+      "geoId": "1463513",
+      "documentId": "1463513",
+      "trackingItems": "hotel",
+      "secondaryText": "Dharamsala, Himachal Pradesh, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 960,
           "maxWidth": 1280,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/2d/19/9d/the-property-itself.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Kashmir Treat Group of Houseboats</b>",
-        "geoId": "11549406",
-        "documentId": "11549406",
-        "trackingItems": "hotel",
-        "secondaryText": "Srinagar, Kashmir, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Kashmir Treat Group of Houseboats</b>",
+      "geoId": "11549406",
+      "documentId": "11549406",
+      "trackingItems": "hotel",
+      "secondaryText": "Srinagar, Kashmir, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 1500,
           "maxWidth": 2000,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/ac/86/5f/kashmir-treat-group-of.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Kashmir Cottage</b>",
-        "geoId": "9553420",
-        "documentId": "9553420",
-        "trackingItems": "hotel",
-        "secondaryText": "Pahalgam, Kashmir, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Kashmir Cottage</b>",
+      "geoId": "9553420",
+      "documentId": "9553420",
+      "trackingItems": "hotel",
+      "secondaryText": "Pahalgam, Kashmir, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 608,
           "maxWidth": 1333,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/c9/82/09/img-20190702-184636-largejpg.jpg?w=700&h=500&s=1"
-        }
       },
-      {
-        "title": "<b>Kashmir Mist Resort</b>",
-        "geoId": "4828642",
-        "documentId": "4828642",
-        "trackingItems": "hotel",
-        "secondaryText": "Sulthan Bathery, India",
-        "image": {
+      "fav": 0,
+      "cart": 0
+  },
+  {
+      "title": "<b>Kashmir Mist Resort</b>",
+      "geoId": "4828642",
+      "documentId": "4828642",
+      "trackingItems": "hotel",
+      "secondaryText": "Sulthan Bathery, India",
+      "image": {
           "__typename": "AppPresentation_PhotoItemSizeDynamic",
           "maxHeight": 3264,
           "maxWidth": 2448,
           "url": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/42/fc/b0/view-of-the-cottage.jpg?w=700&h=500&s=1"
-        }
-      }
-      
-    ];
-  
- 
-      
+      },
+      "fav": 0,
+      "cart": 0
+  }
+]
--- Homepage/displaydata.js
+++ Homepage/displaydata.js
@@ -1,18 +1,26 @@
 let squery=localStorage.getItem("squery");
-let newData=data.filter(element=>{
+let newData=[];
+newData=data.filter(element=>{
     return element.title.toLowerCase().includes(squery);
 })
+let trips=JSON.parse(localStorage.getItem("trips"))||[];
 console.log(newData);
-newData.forEach(element => {
+newData.forEach((element,index) => {
     let box=document.createElement('div');
     let box1=document.createElement('div');
     let box2=document.createElement('div');
     let image=document.createElement('img');
     let name=document.createElement('h3');
+    let n=document.createElement('h4');
     let p1=document.createElement('p');
     let p2=document.createElement('p');
     let p3=document.createElement('p');
 
+    name.setAttribute('style',"display:inline;")
+    n.classList.add("heart");
+         n.innerHTML='<i class="fa-regular fa-heart"></i>';
+   
+    
     image.setAttribute('src',element.image.url)
     name.innerHTML=element.title;
     p1.innerText=element.secondaryText;
@@ -22,6 +30,20 @@ newData.forEach(element => {
 
     box1.append(image);
     box2.append(name,p1,p2,p3);
-    box.append(box1,box2);
+    box.append(n,box1,box2);
     document.getElementById('display').append(box);
+
+    n.addEventListener('click',()=>{
+        
+        n.innerHTML='<i class="fa-solid fa-heart"></i>';
+            for (let i in trips) {
+              if (trips[i] == element) {
+                return alert("Product Already in Trips\nFavourite Section" )
+              }
+            }
+            trips.push(element);
+            localStorage.setItem('trips', JSON.stringify(trips));
+            alert("Product Added To Trips");
+          
+    })
 });
